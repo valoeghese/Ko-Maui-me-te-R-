c33348659d5745d67e568c39bcca2014ce6b0b5c
valoeghese <valoeghesemods@gmail.com>
2021-07-03 11:27:13 +1300

    autogenerated workspace

---
+++ src/main/java/valoeghese/metera/Metera.java
@@ -0,0 +1,17 @@
+package valoeghese.metera;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Random;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Metera implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Metera");
+
+	@Override
+	public void onInitialize() {
+		// Add init code here!
+	}
+}

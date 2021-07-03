089267711ed1d624c3da0c6b0a2e3b3af39e5615
valoeghese <valoeghesemods@gmail.com>
2021-07-03 20:00:40 +1300

    start

--- src/main/java/valoeghese/metera/Metera.java
+++ src/main/java/valoeghese/metera/Metera.java
@@ -1,17 +1,23 @@
 package valoeghese.metera;
 
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
-import java.util.Random;
+import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.registry.Registry;
 
 public class Metera implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger("Metera");
 
 	@Override
 	public void onInitialize() {
-		// Add init code here!
+		EntityType<TeRaa> teRaa = Registry.register(Registry.ENTITY_TYPE, "metera:te_raa", FabricEntityTypeBuilder.create(SpawnGroup.AMBIENT, RedTailedTropicBirdEntity::new)
+				.dimensions(EntityDimensions.fixed(0.4f, 0.8f))
+				.build());
+		Defa
 	}
 }
---
+++ src/main/java/valoeghese/metera/TeRaa.java
@@ -0,0 +1,81 @@
+package valoeghese.metera;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.FollowTargetGoal;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.FlyingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FireballEntity;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
+
+public class TeRaa extends FlyingEntity {
+	public TeRaa(EntityType<? extends FlyingEntity> entityType, World world) {
+		super(entityType, world);
+		this.setNoGravity(true);
+	}
+
+	@Override
+	protected void initGoals() {
+		this.goalSelector.add(7, new ShootFireballGoal());
+		this.targetSelector.add(1, new FollowTargetGoal<>(this, PlayerEntity.class, 10, true, false, (livingEntity) -> {
+			return Math.abs(livingEntity.getY() - this.getY()) <= 4.0D; // todo only shoot those who have roped them
+		}));
+	}
+
+	@Override
+	public void tick() {
+		this.setVelocity(0.0, 1.0, 0.0); // todo the sun rises
+		super.tick();
+	}
+
+	@Override
+	public boolean handleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource) {
+		return false;
+	}
+
+	private class ShootFireballGoal extends Goal {
+		public int cooldown;
+
+		public boolean canStart() {
+			return TeRaa.this.getTarget() != null;
+		}
+
+		public void start() {
+			this.cooldown = 0;
+		}
+
+		public void tick() {
+			LivingEntity livingEntity = TeRaa.this.getTarget();
+			double d = 64.0D;
+			if (livingEntity.squaredDistanceTo(TeRaa.this) < 2048.0D && TeRaa.this.canSee(livingEntity)) {
+				World world = TeRaa.this.world;
+				++this.cooldown;
+				if (this.cooldown == 10 && !TeRaa.this.isSilent()) {
+					world.syncWorldEvent((PlayerEntity)null, WorldEvents.GHAST_WARNS, TeRaa.this.getBlockPos(), 0);
+				}
+
+				if (this.cooldown == 20) {
+					double e = 4.0D;
+					Vec3d vec3d = TeRaa.this.getRotationVec(1.0F);
+					double f = livingEntity.getX() - (TeRaa.this.getX() + vec3d.x * 4.0D);
+					double g = livingEntity.getBodyY(0.5D) - (0.5D + TeRaa.this.getBodyY(0.5D));
+					double h = livingEntity.getZ() - (TeRaa.this.getZ() + vec3d.z * 4.0D);
+					if (!TeRaa.this.isSilent()) {
+						world.syncWorldEvent((PlayerEntity)null, WorldEvents.GHAST_SHOOTS, TeRaa.this.getBlockPos(), 0);
+					}
+
+					FireballEntity fireballEntity = new FireballEntity(world, TeRaa.this, f, g, h, 6);
+					fireballEntity.setPosition(TeRaa.this.getX() + vec3d.x * 4.0D, TeRaa.this.getBodyY(0.5D) + 0.5D, fireballEntity.getZ() + vec3d.z * 4.0D);
+					world.spawnEntity(fireballEntity);
+					this.cooldown = -40;
+				}
+			} else if (this.cooldown > 0) {
+				--this.cooldown;
+			}
+		}
+	}
+}
